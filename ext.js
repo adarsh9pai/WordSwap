6126c7b5eb61df5145314440351f880c52576716
Adarsh Pai <adarsh9pai@gmail.com>
2018-03-02 15:19:27 -0600

    Added code

--- ext.js
+++ ext.js
@@ -1,1 +1,17 @@
-//init
+const words=document.getElementsByTagName("*");
+for(var x=0;x<words.length;x++)
+{
+    var word=words[x];
+    for(var y=0;y<word.childNodes.length;y++)
+    {
+        var nd=word.childNodes[y];
+        if(nd.nodeType===3)//actual text of element
+        {
+            var txt=nd.nodeValue;
+            var rtxt=txt.replace("Blockchain","Glorified LinkedList");
+            if(rtxt!==txt){
+                element.replaceChild(document.createTextNode(rtxt),nd);
+            }
+        }
+    }
+}
