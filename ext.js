31ff745d5f7af4bce27c56e2b65426b1b14fb304
Adarsh Pai <adarsh9pai@gmail.com>
2018-03-02 15:33:03 -0600

    Done.

--- ext.js
+++ ext.js
@@ -1,16 +1,15 @@
-const words=document.getElementsByTagName("*");
-for(var x=0;x<words.length;x++)
-{
-    var word=words[x];
-    for(var y=0;y<word.childNodes.length;y++)
-    {
-        var nd=word.childNodes[y];
-        if(nd.nodeType===3)//actual text of element
-        {
-            var txt=nd.nodeValue;
-            var rtxt=txt.replace("Blockchain","Glorified LinkedList");
-            if(rtxt!==txt){
-                element.replaceChild(document.createTextNode(rtxt),nd);
+var words = document.getElementsByTagName('*');
+for (var x = 0; x < words.length; x++) {
+    var word = words[x];
+    for (var y = 0; y < word.childNodes.length; y++) {
+        var nd = word.childNodes[y];
+        if (nd.nodeType === 3)//actual text of attr
+         {
+            var text = nd.nodeValue;
+            text=text.toLowerCase();
+            var rtext= text.replace("blockchain", 'mulitple copies of a giant Excel spreadsheet');
+            if (rtext !== text) {
+                word.replaceChild(document.createTextNode(rtext), nd);
             }
         }
     }
